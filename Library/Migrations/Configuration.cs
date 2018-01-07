18ccce27254d6ba6ee4ab4d9a9c0f450cda93e45
piters3 <piter.s3335@gmail.com>
2018-01-07 01:02:01 +0100

    MDF, Mail

--- Library/Migrations/Configuration.cs
+++ Library/Migrations/Configuration.cs
@@ -1,33 +1,63 @@
 namespace Library.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using Library.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Library.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
-            ContextKey = "Library.Models.ApplicationDbContext";
+            //ContextKey = "Library.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(Library.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            UsersAndRoles(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private static void UsersAndRoles(ApplicationDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(roleStore);
+                var roleAdmin = new IdentityRole { Name = "Admin" };
+                var roleUser = new IdentityRole { Name = "user" };
+
+                manager.Create(roleAdmin);
+                manager.Create(roleUser);
+            }
+
+            if (!context.Users.Any())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new ApplicationUserManager(userStore);
+
+                var admin = new ApplicationUser
+                {
+                    UserName = "admin",
+                    Email = "admin@admin.com",
+                    EmailConfirmed = true
+                };
+
+                userManager.Create(admin, "admin");
+                //userManager.AddToRole(admin.Id, "Admin");
+
+                var user = new ApplicationUser
+                {
+                    UserName = "user",
+                    Email = "user@user.com",
+                    EmailConfirmed = true
+                };
+
+                userManager.Create(user, "user");
+                //userManager.AddToRole(user.Id, "user");
+            }
         }
     }
 }
